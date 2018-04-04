5dc9add343c474ac40f5fc008fc30aa1175d1bad
Terrance Corley <terrancecorley@outlook.com>
2018-04-04 12:28:43 -0700

    Added part 2 of arrOfObjs question to repo

--- arrOfObjs.js
+++ arrOfObjs.js
@@ -7,18 +7,31 @@
 const arr = [
 	{
 		name: 'Steve Jobs',
-		jobTitle: 'CEO',
+		jobTitle: 'Boss',
 	},
 	{
 		name: 'Drake',
-		jobTitle: 'Musician',
+		jobTitle: 'Clerk',
+		boss: 'Steve Jobs',
 	},
 	{
 		name: 'Cassandra C.',
-		jobTitle: 'Author',
+		jobTitle: 'Custodian',
+		boss: 'Steve Jobs',
 	},
 ];
 
 for (let item in arr) {
-	console.log(`${arr[item].name}, ${arr[item].jobTitle}`);
+
+	if (arr[item].boss === undefined) {
+		console.log(`Boss ${arr[item].name} doesn't report to anybody.`);
+	} else {
+		console.log(`${arr[item].name}, ${arr[item].jobTitle}, reports to ${arr[item].boss}.`);
+	}
 }
+
+// Expand on the previous example by adding a boss property to everyone except the owner of the company.
+// Change the iteration to print out messages in this format: "${title} ${name} reports to ${boss}.". For example: Junior Engineer Bob reports to Fred..
+// What gets printed out for the owner?
+// Adjust the message so that people with no boss display "${title} ${name} doesn't report to anybody." - for example, Founder John doesn't report to anybody.
+
