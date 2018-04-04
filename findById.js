9d9ccbb67587143e7a682f2ba1466addac0bc510
Dominick McKoy <dominick.mckoy@gmail.com>
2018-04-04 12:21:07 -0400

    Added findById function

---
+++ findById.js
@@ -0,0 +1,31 @@
+'use strict';
+const findById = (items, idNum) => items.find(item => item.id===idNum);
+
+
+function testIt() {
+  const testData = [
+    { id: 1, foo: 'bar' },
+    { id: 2, foo: 'bizz' },
+    { id: 3, bang: 'boo' },
+  ];
+  const result = findById(testData, 3);
+  if (!(result && result !== null && typeof result === 'object')) {
+    console.error('`findById` must return an object');
+    return;
+  }
+  if (result.id !== 3) {
+    console.error(
+      'Asked for item with id of `3` but got back one with id of ' + result.id
+    );
+    return;
+  }
+  if (result.bang !== 'boo') {
+    console.error(
+      'Expected all key/value pairs from target object to be returned'
+    );
+    return;
+  }
+  console.log('SUCCESS: `findByid` is working');
+}
+
+testIt();
